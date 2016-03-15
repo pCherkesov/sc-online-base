d4728326002b9b26679294958029b43af77e8581
pCherkesov <pcherkesov@gmail.com>
2016-03-15 13:45:49 +0300

    +Смена статуса при отправке СМС

--- ajax/send_sms.php
+++ ajax/send_sms.php
@@ -19,4 +19,19 @@ catch (Exception $e) {
 $result_query = "INSERT INTO `".$S_CONFIG['prefix']."sms` VALUE(0, ".$idr.", CURRENT_TIMESTAMP, '".$msg."', 1, 0);";
 
 $results = mysqli_query($S_CONFIG['link'], $result_query) or exit(mysql_error());
+
+//====================================================
+
+$query = "SELECT `string` FROM `".$S_CONFIG['prefix']."remont` WHERE `id_r` = ".$idr;
+
+$result = mysqli_query($S_CONFIG['link'], $query) or exit(mysqli_error($S_CONFIG['link']));
+while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+	$status = $line['string'];
+}
+
+$status[9] == "Y";
+
+$query1 = "UPDATE `".$S_CONFIG['prefix']."remont` SET `string` = '".$status."' WHERE `id_r` = ".$idr." LIMIT 1";
+
+$r1 = mysqli_query($S_CONFIG['link'], $query1) or exit(mysqli_error($S_CONFIG['link']));
 ?>
