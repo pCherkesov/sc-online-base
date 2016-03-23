bb03faea97f31a2f38406cde78a8e5fb164c6737
pCherkesov <pcherkesov@gmail.com>
2016-03-23 23:19:03 +0300

    +Открыта в меню статистика за месяц *Доработки по статистике

--- actions/stat/month.php
+++ actions/stat/month.php
@@ -26,32 +26,35 @@ $result = mysqli_query($S_CONFIG['link'], $query) or exit(mysql_error($S_CONFIG[
 while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {
 	$work[] = $line['id_r'];
 }
-//======================================================
-$query = "SELECT r.id_r, DATE_FORMAT(r.date, '%d.%m.%Y') AS date, b.brand, m.model, 
-c.client, r.client_fio, r.serial, r.string, r.complete, DATE_FORMAT(r.date_complete, '%d.%m.%Y') AS date_complete  
-FROM `".$S_CONFIG['prefix']."remont` AS r, `".$S_CONFIG['prefix']."model` AS m,
- `".$S_CONFIG['prefix']."client` AS c, `".$S_CONFIG['prefix']."brand` AS b
-WHERE m.id_brand=b.id_brand and r.id_client=c.id_client and r.id_model=m.id_model and r.hidden='N' and r.id_r IN (".join(", ", $work).") ORDER BY r.date_complete ASC";
 
-$result = mysqli_query($S_CONFIG['link'], $query) or exit(mysql_error($S_CONFIG['link']));
-$total_price = 0;
-while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-//=======================================================================
-	$result_query="SELECT id, DATE_FORMAT(date, '%d.%m.%Y') AS date, text, price, id_worker 
-					FROM `".$S_CONFIG['prefix']."work`
-					WHERE id_r=".$line['id_r']." and hidden='N'";
-					
-	$results = mysqli_query($S_CONFIG['link'], $result_query) or exit(mysql_error($S_CONFIG['link']));
-	// Печать результатов в HTML
-	$details = array();
-	while ($lines = mysqli_fetch_array($results, MYSQL_ASSOC)) {
-		$details[] = $lines;
-		$total_price = $total_price + $lines['price'];
+if(isset($work)) {
+	//======================================================
+	$query = "SELECT r.id_r, DATE_FORMAT(r.date, '%d.%m.%Y') AS date, b.brand, m.model, r.id_worker, 
+	c.client, r.client_fio, r.serial, r.string, r.complete, DATE_FORMAT(r.date_complete, '%d.%m.%Y') AS date_complete  
+	FROM `".$S_CONFIG['prefix']."remont` AS r, `".$S_CONFIG['prefix']."model` AS m,
+	 `".$S_CONFIG['prefix']."client` AS c, `".$S_CONFIG['prefix']."brand` AS b
+	WHERE m.id_brand=b.id_brand and r.id_client=c.id_client and r.id_model=m.id_model and r.hidden='N' and r.id_r IN (".join(", ", $work).") ORDER BY r.date_complete ASC";
+
+	$result = mysqli_query($S_CONFIG['link'], $query) or exit(mysql_error($S_CONFIG['link']));
+	$total_price = 0;
+	while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+		$line['string'] = str_split($line['string']);
+
+		$result_query="SELECT id, DATE_FORMAT(date, '%d.%m.%Y') AS date, text, price, id_worker 
+						FROM `".$S_CONFIG['prefix']."work`
+						WHERE id_r=".$line['id_r']." and hidden='N'";
+						
+		$results = mysqli_query($S_CONFIG['link'], $result_query) or exit(mysql_error($S_CONFIG['link']));
+
+		$details = array();
+		while ($lines = mysqli_fetch_array($results, MYSQL_ASSOC)) {
+			$details[] = $lines;
+			$total_price = $total_price + $lines['price'];
+		}
+		$line['details'] = $details; 
+		$data[] = $line;
 	}
-	$line['details'] = $details; 
-	$data[] = $line;
 }
-
 // var_dump($data);
 // var_dump($total_price);
 $query = "SELECT * FROM `".$S_CONFIG['prefix']."worker`";
